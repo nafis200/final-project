10a99e4e41ad403d0e8ecfa0d415d94e95b35742
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-16 22:49:13 +0600

    menu dessert

--- src/components1/Menu.jsx
+++ src/components1/Menu.jsx
@@ -2,16 +2,32 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../components/Cover";
 import menuImg from "../images/menu/banner3.jpg";
 import PopularMenu from "../components/PopularMenu";
+
+import useMenu from "./hooks/useMenu";
+import Sectiontitle from "../components/Sectiontitle";
+import Menucategory from "./Menucategory";
 const Menu = () => {
+  const [menu] = useMenu()
+ 
+  const dessert = menu.filter(item => item.category === 'dessert')
+  const soup  = menu.filter(item => item.category === 'soup')
+  const salad  = menu.filter(item => item.category === 'salad')
+  const pizza = menu.filter(item => item.category === 'pizza')
+  const offered = menu.filter(item => item.category === 'offered')
+ 
   return (
     <div>
       <Helmet>
         <title>Bistro Boss menu</title>
       </Helmet>
-      <Cover menuImg={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover menuImg={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
+      
+      {/* <PopularMenu></PopularMenu> */}
+      title && <Cover menuImg={menuImg} title="Our Menu"></Cover>
+      <Sectiontitle subHeading="Dont miss" heading="today's Offer"></Sectiontitle>
+      <Menucategory items={offered}></Menucategory>
+      <Menucategory items={dessert} title="dessert"></Menucategory>
+      
+
     </div>
   );
 };
--- src/components1/Menucategory.jsx
+++ src/components1/Menucategory.jsx
@@ -1,13 +1,19 @@
+import Cover from "../components/Cover";
+import Menuitem from "../components/Menuitem";
 
-
-
-
-const Menucategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Menucategory = ({ items,title,coverImg }) => {
+  return (
+    <div>
+    {
+        title && <Cover menuImg={coverImg} title="Our Menu"></Cover>
+    }
+      <div className="grid md:grid-cols-2 gap-4 lg:grid-cols-3 grid-cols-1 mb-12">
+        {items.map((item) => (
+          <Menuitem key={item._id} item={item}></Menuitem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Menucategory;
