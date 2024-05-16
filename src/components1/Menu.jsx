7b359e0edb922ff7990d5856eb572c01017ed0dc
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-16 23:14:15 +0600

    items add

--- src/components1/Menu.jsx
+++ src/components1/Menu.jsx
@@ -2,7 +2,10 @@ import { Helmet } from "react-helmet-async";
 import Cover from "../components/Cover";
 import menuImg from "../images/menu/banner3.jpg";
 import dessertImg from '../images/menu/dessert-bg.jpeg'
+import pizzaImg from '../images/menu/pizza-bg.jpg'
+import saladImg from '../images/menu/salad-bg.jpg'
 // import PopularMenu from "../components/PopularMenu";
+import soupImg from '../images/menu/soup-bg.jpg'
 
 import useMenu from "./hooks/useMenu";
 import Sectiontitle from "../components/Sectiontitle";
@@ -23,10 +26,14 @@ const Menu = () => {
       </Helmet>
       
       {/* <PopularMenu></PopularMenu> */}
-      title && <Cover menuImg={menuImg} title="Our Menu"></Cover>
+      <Cover menuImg={menuImg} title="Our Menu"></Cover>
       <Sectiontitle subHeading="Dont miss" heading="today's Offer"></Sectiontitle>
       <Menucategory items={offered}></Menucategory>
+
       <Menucategory img={dessertImg} items={dessert} title="dessert"></Menucategory>
+      <Menucategory  img={pizzaImg} items={pizza} title="Pizza"></Menucategory>
+      <Menucategory img={saladImg} title="salads" items={salad}></Menucategory>
+      <Menucategory img={soupImg} title="Soups" items={soup}></Menucategory>
       
 
     </div>
--- src/components1/Menucategory.jsx
+++ src/components1/Menucategory.jsx
@@ -3,11 +3,11 @@ import Menuitem from "../components/Menuitem";
 
 const Menucategory = ({ items,title,img }) => {
   return (
-    <div>
+    <div className="pt-8">
     {
-        title && <Cover menuImg={img} title="Our Menu"></Cover>
+        title && <Cover menuImg={img} title={title}></Cover>
     }
-      <div className="grid md:grid-cols-2 gap-4 lg:grid-cols-3 grid-cols-1 mb-12">
+      <div className="grid md:grid-cols-2 gap-4 lg:grid-cols-3 grid-cols-1 mb-12 mt-16">
         {items.map((item) => (
           <Menuitem key={item._id} item={item}></Menuitem>
         ))}
