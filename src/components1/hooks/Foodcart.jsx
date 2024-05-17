8c653f21cb6f279b7a6258292d6ef465773da61a
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-17 18:15:52 +0600

    final project -part-2

--- src/components1/hooks/Foodcart.jsx
+++ src/components1/hooks/Foodcart.jsx
@@ -9,7 +9,7 @@ const Foodcart = ({ item }) => {
         />
       </figure>
       <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4">${price}</p>
-      <div className="card-body text-center">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
