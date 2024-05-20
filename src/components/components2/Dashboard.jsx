3dde8ae2f1da6e18e7916cb2120b2d0bcb4ed73d
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-20 16:00:37 +0600

    dashboard start

--- src/components/components2/Dashboard.jsx
+++ src/components/components2/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaShoppingCart } from "react-icons/fa";
+import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 
@@ -11,9 +11,19 @@ const Dashboard = () => {
 
             <ul className="menu p-4">
                <li> 
-              
+                 <NavLink to="/dashboard/userHome"> <FaHome></FaHome> user Home</NavLink> 
+                </li>
+               <li> 
                  <NavLink to="/dashboard/cart"> <FaShoppingCart> </FaShoppingCart> My cart</NavLink> 
-                 
+                </li>
+               <li> 
+                 <NavLink to="/dashboard/reservation"> <FaCalendar></FaCalendar>Reservation</NavLink> 
+                </li>
+               <li> 
+                 <NavLink to="/dashboard/review"> <FaCalendar></FaCalendar>Review</NavLink> 
+                </li>
+               <li> 
+                 <NavLink to="/dashboard/bookings"> <FaCalendar></FaCalendar>Bookings</NavLink> 
                 </li>
             </ul>
 
