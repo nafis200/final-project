5c159a42c2454567b59517d479725cd2c4ea02d4
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-22 22:47:51 +0600

    add

--- src/admin/Allusers.jsx
+++ src/admin/Allusers.jsx
@@ -1,53 +1,87 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../components1/hooks/useAxiosSecure";
-
-
+import Swal from "sweetalert2";
 
 const Allusers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [],refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    }
+  });
 
-    const axiosSecure = useAxiosSecure()
-    const {data: users = []} = useQuery({
-        queryKey:['users'],
-        queryFn: async()=>{
-          const res = await axiosSecure.get('/users')
-          return res.data;
+   const handleMakeAdmin = user =>{
+    
+   }
+
+  const handleDelete = (user)=>{
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            axiosSecure.delete(`/users/${user._id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
         }
-    })
-    return (
-        <div>
+    });
+  }
 
-           <div className="flex justify-evenly my-4">
-            <h2 className="text-3xl">All users</h2>
-            <h2 className="text-3xl">total users :{users.length}</h2>
-           </div>
+  return (
+    <div>
+      <div className="flex justify-evenly my-4">
+        <h2 className="text-3xl">All users</h2>
+        <h2 className="text-3xl">total users :{users.length}</h2>
+      </div>
 
-           <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        users.map(user => <tr key={user._id}>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
-          </tr> )
-      }
-      
-    </tbody>
-  </table>
-</div>
-
-        </div>
-    );
+      <div className="overflow-x-auto">
+        <table className="table table-zebra w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Role</td>
+                <td>Action</td>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user,index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                <button onClick={()=> handleMakeAdmin(user)} className="btn btn-ghost btn-xs text-orange-400">AX</button>
+                </td>
+                <td>
+                <button onClick={()=> handleDelete(user)} className="btn btn-ghost btn-xs">UX</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Allusers;
