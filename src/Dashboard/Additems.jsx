62c39816bba652f93a47d3aad7215a80d672a54c
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-24 14:14:16 +0600

    module69-2

--- src/Dashboard/Additems.jsx
+++ src/Dashboard/Additems.jsx
@@ -1,11 +1,34 @@
-
+import Sectiontitle from "../components/Sectiontitle";
+import { useForm } from "react-hook-form";
 
 const Additems = () => {
-    return (
-        <div>
-            <h2 className="2xl">Add Items</h2>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {};
+  return (
+    <div>
+      <Sectiontitle
+        heading="add an items"
+        subHeading="whats new"
+      ></Sectiontitle>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} />
+          <select {...register("category")} className="select select-bordered w-full max-w-xs"> 
+            <option disabled selected>
+             Select a category
+            </option>
+            <option value="salad">Salad</option>
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="dessert">Dessert</option>
+            <option value="drinks">Drinks</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Additems;
