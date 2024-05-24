ccdb91a26b0ff1b15e00210bf2456db457b55019
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-24 19:58:35 +0600

    deete menu

--- src/Dashboard/ManageItem.jsx
+++ src/Dashboard/ManageItem.jsx
@@ -1,6 +1,13 @@
 import Sectiontitle from "../components/Sectiontitle";
+import useMenu from "../components1/hooks/useMenu";
 
 const ManageItem = () => {
+  const [menu] = useMenu()
+  
+  const handleDelete = (item)=>{
+    
+  }
+  
   return (
     <div>
       <Sectiontitle
@@ -12,50 +19,40 @@ const ManageItem = () => {
         <div className="overflow-x-auto">
           <table className="table">
             {/* head */}
+
             <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-                <th></th>
-              </tr>
+            <tr>
+                    <th>#</th>
+                    <th>Image</th>
+                    <th>Item name</th>
+                    <th>Price</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                  </tr>
             </thead>
             <tbody>
-              <tr>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
-                        <img
-                          src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png"
-                          alt="Avatar Tailwind CSS Component"
-                        />
+              {
+                menu.map(item => <tr>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={item.image}
+                            />
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-              </tr>
+                    </td>
+                    <td>
+                      {item.name}
+                    </td>
+                    <td>{item.price}</td>
+                    <th>
+              <button onClick={()=> handleDelete(item._id)} className="btn btn-ghost btn-xs">X</button>
+            </th>
+                  </tr> )
+              }
             </tbody>
           </table>
         </div>
