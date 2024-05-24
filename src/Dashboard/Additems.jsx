eba69864e7379a27600d6c4302a194c4d346807f
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-24 18:21:46 +0600

    image url

--- src/Dashboard/Additems.jsx
+++ src/Dashboard/Additems.jsx
@@ -1,23 +1,38 @@
 import Sectiontitle from "../components/Sectiontitle";
 import { useForm } from "react-hook-form";
 import useAxiospublic from "../components1/hooks/useAxiospublic";
+import useAxiosSecure from "../components1/hooks/useAxiosSecure";
 
 const image_hosting = import.meta.env.VITE_IMAGE_HOSTING_KEY
 
 const image_api = `https://api.imgbb.com/1/upload?key=${image_hosting}`
 
 const Additems = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiospublic()
+  const axiosSecure = useAxiosSecure()
   const onSubmit = async(data) => {
-    console.log(data)
     const imageFile = {image: data.image[0]}
     const res = await axiosPublic.post(image_api,imageFile,{
         headers:{
             'content-type':'multipart/form-data'
         }
     })
-     console.log(res.data)
+     if(res.data.success){
+        const menuItem = {
+           name:data.name,
+           category:data.category,
+           price:parseFloat(data.price),
+           recipe:data.recipe,
+           image:res.data.data.display_url
+        }
+        // 
+        const menuRes = await axiosSecure.post('/menu',menuItem) 
+        console.log(menuRes.data);
+        if(menuRes.data.insertedId){
+              reset()
+        }
+     }
   };
   return (
     <div>
